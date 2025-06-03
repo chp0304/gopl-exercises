dec71897653fe7a8844cd862af65dd35839a0b86
chenchp_mac <15554505052@163.com>
2025-06-03 16:24:57 +0800

    other=add Exercise8.5

---
+++ ch8/Exercise8.5/mandelbrot_parallel/mandelbrot.go
@@ -0,0 +1,115 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 61.
+//!+
+
+// Mandelbrot emits a PNG image of the Mandelbrot fractal.
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"math/cmplx"
+	"os"
+	"runtime"
+	"sync"
+	"time"
+)
+
+type xyz struct {
+	x int
+	y int
+	z color.Color
+}
+
+// parallel excute consume time = 90-100
+func main() {
+	start := time.Now()
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	var wg sync.WaitGroup
+	workers := runtime.GOMAXPROCS(-1)
+	rows := make(chan int)
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for py := range rows {
+				y := float64(py)/height*(ymax-ymin) + ymin
+				for px := 0; px < width; px++ {
+					x := float64(px)/width*(xmax-xmin) + xmin
+					z := complex(x, y)
+					img.Set(px, py, mandelbrot(z))
+					// Image point (px, py) represents complex value z.
+				}
+			}
+		}()
+	}
+
+	for row := 0; row < height; row++ {
+		rows <- row
+	}
+	close(rows)
+
+	wg.Wait()
+
+	png.Encode(os.Stdout, img) // NOTE: ignoring errors
+	fmt.Println()
+	fmt.Printf("consume time = %dms\n", time.Since(start).Milliseconds())
+}
+
+func mandelbrot(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			return color.Gray{255 - contrast*n}
+		}
+	}
+	return color.Black
+}
+
+//!-
+
+// Some other interesting functions:
+
+func acos(z complex128) color.Color {
+	v := cmplx.Acos(z)
+	blue := uint8(real(v)*128) + 127
+	red := uint8(imag(v)*128) + 127
+	return color.YCbCr{192, blue, red}
+}
+
+func sqrt(z complex128) color.Color {
+	v := cmplx.Sqrt(z)
+	blue := uint8(real(v)*128) + 127
+	red := uint8(imag(v)*128) + 127
+	return color.YCbCr{128, blue, red}
+}
+
+// f(x) = x^4 - 1
+//
+// z' = z - f(z)/f'(z)
+//
+//	= z - (z^4 - 1) / (4 * z^3)
+//	= z - (z - 1/z^3) / 4
+func newton(z complex128) color.Color {
+	const iterations = 37
+	const contrast = 7
+	for i := uint8(0); i < iterations; i++ {
+		z -= (z - 1/(z*z*z)) / 4
+		if cmplx.Abs(z*z*z*z-1) < 1e-6 {
+			return color.Gray{255 - contrast*i}
+		}
+	}
+	return color.Black
+}
